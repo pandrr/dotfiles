4ca9f0f8dfe6f07af5b99713ad34b339602d2035
tom <tom@undev.de>
2020-11-09 07:57:32 +0100

    mac key bindings

---
+++ keys.py
@@ -0,0 +1,12 @@
+import re
+from xkeysnail.transform import *
+
+define_keymap(None, {
+    K("Super-c"): K("C-c"),
+    K("Super-v"): K("C-v"),
+    K("Super-t"): K("C-t"),
+    K("Super-q"): K("C-q"),
+    K("Super-w"): K("C-w"),
+    K("Super-a"): K("C-a"),
+    K("Super-n"): K("C-n"),
+})
